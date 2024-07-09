1f0320ec1bb2d75cdfa27e1ce0174beae6a6bce5
shafiquddin <shafiquddin2k@gmail.com>
2024-07-10 01:24:45 +0530

    Loading indiacator for autopopulate

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,56 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx'
+
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+  const { data, isError, isPending, error } = useQuery({
+    queryKey: ['events', { id: params.id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  })
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) { }
 
   function handleClose() {
     navigate('../');
   }
 
+  let content;
+
+  if (isPending) {
+    content = <div className='center'>
+      <LoadingIndicator />
+    </div>
+  }
+
+  if (isError) {
+    content = <>
+      <ErrorBlock title="failed to fetch event" message={error.info?.message || 'failed to fetch event, try again later'} />
+      <div className='form-actions'>
+        <Link to='../' className='button'>Okey</Link>
+      </div>
+    </>
+  }
+
+  if (data) {
+    content = <EventForm inputData={data} onSubmit={handleSubmit}>
+      <Link to="../" className="button-text">
+        Cancel
+      </Link>
+      <button type="submit" className="button">
+        Update
+      </button>
+    </EventForm>
+  }
+
   return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
-    </Modal>
+    <Modal onClose={handleClose}>{content}</Modal>
   );
 }
